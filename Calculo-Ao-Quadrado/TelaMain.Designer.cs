3733af82faeb6270757940ef98d61baf42ac8302
diego.hncosta <diego.hncosta@SBC0505219W10-1>
2022-04-26 11:59:27 -0300

    Adicionar arquivos de projeto.

---
+++ Calculo-Ao-Quadrado/TelaMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace Calculo_Ao_Quadrado
+{
+    partial class TelaMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnCalculo = new System.Windows.Forms.Button();
+            this.tbNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(482, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculando os Valores ao Quadrado";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(65, 127);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(106, 15);
+            this.lblNumero.TabIndex = 1;
+            this.lblNumero.Text = "Digite um Número";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblResultado.Location = new System.Drawing.Point(65, 227);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(380, 179);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculo
+            // 
+            this.btnCalculo.Location = new System.Drawing.Point(370, 156);
+            this.btnCalculo.Name = "btnCalculo";
+            this.btnCalculo.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculo.TabIndex = 3;
+            this.btnCalculo.Text = "Calcular";
+            this.btnCalculo.UseVisualStyleBackColor = true;
+            this.btnCalculo.Click += new System.EventHandler(this.btnCalculo_Click);
+            // 
+            // tbNumero
+            // 
+            this.tbNumero.Location = new System.Drawing.Point(65, 157);
+            this.tbNumero.Name = "tbNumero";
+            this.tbNumero.Size = new System.Drawing.Size(255, 23);
+            this.tbNumero.TabIndex = 4;
+            // 
+            // TelaMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 450);
+            this.Controls.Add(this.tbNumero);
+            this.Controls.Add(this.btnCalculo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "TelaMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Laboratório - Cálculo";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TelaMain_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnCalculo;
+        private System.Windows.Forms.TextBox tbNumero;
+    }
+}
---
+++ Calculo-Ao-Quadrado/TelaMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_Ao_Quadrado
+{
+    public partial class TelaMain : Form
+    {
+        public TelaMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCalculo_Click(object sender, EventArgs e)
+        {
+            int numero = 0;
+            int quadrado = 0;
+
+            numero = Convert.ToInt32(tbNumero.Text);
+
+            quadrado = numero * numero;
+
+            lblResultado.Text = "O número " + numero + " ao quadrado, é " + quadrado;
+
+            tbNumero.Focus();
+
+            tbNumero.SelectAll();
+
+
+        }
+
+        private void TelaMain_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                btnCalculo.PerformClick();
+            }
+        }
+    }
+}
